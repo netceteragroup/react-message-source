6fc2dce37e6359269133a6ff259ebf3ccba1ab08
Oliver Lazoroski <oliver.lazoroski@netcetera.com>
2019-03-15 23:34:36 +0100

    messageSource -> withMessages

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@
 export { Provider } from './lib/MessageSourceContext';
 export { FetchingProvider } from './lib/FetchingProvider';
 export { useMessageSource } from './lib/useMessageSource';
-export { withMessages, propTypes } from './lib/messageSource';
+export { withMessages, propTypes } from './lib/withMessages';
